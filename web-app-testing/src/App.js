84d6b3bb4ca64e0f05b3a2e065e492b50df2db60
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-03 15:50:25 -0400

    Display component displays initial state for ball and strike

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
+
 
 function App() {
+
+  const [ball, setBall] = useState(0);
+  const [strike, setStrike] = useState(0);
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display ball={ball} strike={strike}/>
+      <Dashboard />
     </div>
   );
 }
